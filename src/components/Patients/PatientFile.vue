347e309ce91fa1db9a828a00acdf4c1aef61c020
rangata <urandom@abv.bg>
2019-07-25 15:06:56 +0300

    first commit

---
+++ src/components/Patients/PatientFile.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  name: 'PatientFile',
+
+  data() {
+    return {
+      currentPatient: '',
+    };
+  },
+  mounted() {
+    console.log(this.$props.id);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Patients/PatientTableComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'PatientTableComponent'
+  };
+</script>
+
+<style scoped>
+
+</style>
